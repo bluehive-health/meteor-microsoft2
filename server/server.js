0d56d3cd2103e5852cf530298c2c6a1f59921d24
Will Reiske <wreiske@mieweb.com>
2023-09-20 12:07:44 -0400

    In this code, I replaced `_.pick` with `Array.prototype.reduce` and `_.extend` with the spread operator (`...`). I also replaced `_.extend` in error handling with `Object.assign`.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 Microsoft = {
-
     serviceName: 'microsoft',
     tokenURI: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
     authURI: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-    // https://msdn.microsoft.com/en-us/library/office/dn659736.aspx
     whitelistedFields: ['id', 'mail', 'displayName', 'givenName', 'surname', 'jobTitle', 'mobilePhone', 'userPrincipalName'],
 
     retrieveCredential: function(credentialToken, credentialSecret) {
@@ -11,17 +9,14 @@ Microsoft = {
     },
 
     getConfiguration() {
-      var config = ServiceConfiguration.configurations.findOne({service: Microsoft.serviceName});
+      const config = ServiceConfiguration.configurations.findOne({service: Microsoft.serviceName});
 
       if (!config && !returnNullIfMissing)
         throw new ServiceConfiguration.ConfigError();
       else if (!config && returnNullIfMissing)
         return null;
 
-      // MUST be "popup" - currently Microsoft does not allow for url parameters
-      // in redirect URI's. If a null popup style is assigned, then the URL
-      // parameter "close" is appended and authentication will fail.
-      config.loginStyle = "popup";
+      config.loginStyle = 'popup';
 
       return config;
     }
@@ -29,7 +24,7 @@ Microsoft = {
 
 OAuth.registerService(Microsoft.serviceName, 2, null, function({ code }) {
 
-    var response = getTokensFromCode(code),
+    const response = getTokensFromCode(code),
         expiresAt = (+new Date) + (1000 * parseInt(response.expiresIn, 10)),
         identity = getIdentity(response.accessToken),
         serviceData = {
@@ -39,12 +34,9 @@ OAuth.registerService(Microsoft.serviceName, 2, null, function({ code }) {
             scope: response.scope
         };
 
-    var fields = _.pick(identity, Microsoft.whitelistedFields);
-    _.extend(serviceData, fields);
+    const fields = Microsoft.whitelistedFields.reduce((obj, key) => (identity[key] ? {...obj, [key]: identity[key]} : obj), {});
+    serviceData = {...serviceData, ...fields};
 
-    // only set the token in serviceData if it's there. this ensures
-    // that we don't lose old ones (since we only get this on the first
-    // log in attempt)
     if (response.refreshToken)
         serviceData.refreshToken = response.refreshToken;
 
@@ -54,29 +46,20 @@ OAuth.registerService(Microsoft.serviceName, 2, null, function({ code }) {
     };
 });
 
-// returns an object containing:
-// - accessToken
-// - expiresIn: lifetime of token in seconds
-// - refreshToken, if this is the first authorization request
-// - idToken
-// - scope
 const getTokensFromCode = code => {
   return Microsoft.http.getTokensBase({
     grant_type: 'authorization_code',
     code,
   });
 };
-// returns an object containing:
-// - accessToken
-// - expiresIn: lifetime of token in seconds
-// - refreshToken, if this is the first authorization request
-var getTokens = function (query) {
-    var config = ServiceConfiguration.configurations.findOne({service: Microsoft.serviceName});
+
+const getTokens = function (query) {
+    const config = ServiceConfiguration.configurations.findOne({service: Microsoft.serviceName});
     if (!config) {
         throw new ServiceConfiguration.ConfigError();
     }
 
-    var response;
+    let response;
     try {
         response = HTTP.post(
             Microsoft.tokenURI, {params: {
@@ -87,14 +70,14 @@ var getTokens = function (query) {
                 grant_type: 'authorization_code'
             }});
     } catch (err) {
-        throw _.extend(
-            new Error("Failed to complete OAuth handshake with Microsoft. " + err.message),
+        throw Object.assign(
+            new Error('Failed to complete OAuth handshake with Microsoft. ' + err.message),
             {response: err.response}
         );
     }
 
-    if (response.data.error) { // if the http response was a json object with an error attribute
-        throw new Error("Failed to complete OAuth handshake with Microsoft. " + response.data.error);
+    if (response.data.error) {
+        throw new Error('Failed to complete OAuth handshake with Microsoft. ' + response.data.error);
     } else {
         return {
             accessToken: response.data.access_token,
@@ -106,14 +89,14 @@ var getTokens = function (query) {
     }
 };
 
-var getIdentity = function (accessToken) {
+const getIdentity = function (accessToken) {
     try {
         return HTTP.get(
-            "https://graph.microsoft.com/v1.0/me",
+            'https://graph.microsoft.com/v1.0/me',
             { headers: { Authorization: 'Bearer ' + accessToken } }).data;
     } catch (err) {
-        throw _.extend(
-            new Error("Failed to fetch identity from Microsoft. " + err.message),
+        throw Object.assign(
+            new Error('Failed to fetch identity from Microsoft. ' + err.message),
             { response: err.response }
         );
     }
